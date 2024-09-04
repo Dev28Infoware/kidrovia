cf489fc43ff4da88d2e91a084056f4498c4db4dc
Dev28Infoware <107273689+Dev28Infoware@users.noreply.github.com>
2024-09-04 10:34:16 +0530

    change in param

--- services/SeoService.js
+++ services/SeoService.js
@@ -5,7 +5,7 @@ class SeoService{
     async getSeoTagBasedOnFilter(filter){
         try{
             let response = await csvService.downloadFile('seo');
-            return response.find(element=>element.filter === filter)?response.find(element=>element.filter === filter):new Map();
+            return response.find(element=>element.page === filter)?response.find(element=>element.page === filter):new Map();
             // return result ?result:new Map();
         }
         catch(error){
